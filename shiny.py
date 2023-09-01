10dc3ba15ef3435df94d786225fd1afbc6fbcccb
Anthony Richer <anthony.richer@hootsuite.com>
2023-08-31 20:44:40 -0400

    wip raw dawg

--- shiny.py
+++ shiny.py
@@ -1,11 +1,12 @@
-import weaviate
-import json
-from os import environ
+# import weaviate
+# import json
+# from os import environ
 
-client = weaviate.Client(
-    url = "https://shiny-test-uolmtiuu.weaviate.network",
-    auth_client_secret=weaviate.AuthApiKey(api_key=environ["WEAVIATE_API_KEY"]),  # Replace w/ your Weaviate instance API key
-    additional_headers = {
-        "X-OpenAI-Api-Key": environ["OPENAI_API_KEYs"]  # Replace with your inference API key
-    }
-)
+# client = weaviate.Client(
+#     url = "https://shiny-test-uolmtiuu.weaviate.network",
+#     auth_client_secret=weaviate.AuthApiKey(api_key=environ["WEAVIATE_API_KEY"]),  # Replace w/ your Weaviate instance API key
+#     additional_headers = {
+#         "X-OpenAI-Api-Key": environ["OPENAI_API_KEYs"]  # Replace with your inference API key
+#     }
+# )
+
